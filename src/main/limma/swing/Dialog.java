c764c991d5a4880f58c9687404e1213eca883184
jimmy <jimmy@15187653-a828-0410-8701-c8d75b20fa1b>
2005-10-03 20:43:55 +0000

    the dialogs are now translucent

--- src/main/limma/swing/Dialog.java
+++ src/main/limma/swing/Dialog.java
@@ -14,12 +14,27 @@ public class Dialog extends JPanel {
         setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.white, 1), emptyBorder));
         setBackground(Color.black);
         setVisible(false);
+        setOpaque(false);
     }
 
     public void invalidate() {
         setSize(getPreferredSize());
         setBounds(getParent().getWidth() / 2 - getWidth() / 2, getParent().getHeight() / 2 - getHeight() / 2, getWidth(), getHeight());
         super.invalidate();
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        Composite oldComposite = g2d.getComposite();
+
+        Composite alphaComp = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.8f);
+        g2d.setComposite(alphaComp);
+        g2d.setColor(getBackground());
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+
+        g2d.setComposite(oldComposite);
     }
 
     protected void open() {

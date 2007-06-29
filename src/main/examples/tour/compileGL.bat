9e5f4f0305fcafc11d4eb38606bde5acf5fc275f
jimmy <jimmy@15187653-a828-0410-8701-c8d75b20fa1b>
2007-06-29 21:15:25 +0000

    Starting to work with jogl

--- src/main/examples/tour/compileGL.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Compiling %1 with JOGL...
-javac -classpath "d:\jogl\jogl.jar;d:\jogl\gluegen-rt.jar;." %1
-echo Finished.
--- src/main/examples/tour/runGL.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Executing %1 with JOGL...
-
-java -cp "d:\jogl\jogl.jar;d:\jogl\gluegen-rt.jar;." -Djava.library.path="d:\jogl" -Dsun.java2d.noddraw=true %1 %2
-
-echo Finished.
